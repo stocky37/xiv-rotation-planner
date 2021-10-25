66c60a16f74301e4ddbc1406b6b638a9555dd9b6
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2021-10-25 11:24:18 +0100

    feat: clear rotation when changing jobs
    - has button to manually clear as well

--- src/pages/RotationPage/components/JobActionsPanel/JobActionsPanel.tsx
+++ src/pages/RotationPage/components/JobActionsPanel/JobActionsPanel.tsx
@@ -1,4 +1,4 @@
-import {CircularProgress, Paper} from '@mui/material';
+import {Card, CardContent, CardHeader, CircularProgress, Typography} from '@mui/material';
 import Actions from 'components/Actions';
 import useSelectedJob from 'hooks/useSelectedJob';
 import useUpdateRotation from 'hooks/useUpdateRotation';
@@ -8,9 +8,27 @@ const JobActionsPanel: FC = () => {
 	const {isLoading, data: job} = useSelectedJob();
 	const [appendAction] = useUpdateRotation();
 	return (
-		<Paper elevation={3} sx={{padding: 1, width: '100%'}}>
-			{isLoading ? <CircularProgress /> : <Actions actions={job?.actions} onClick={appendAction} />}
-		</Paper>
+		<Card sx={{width: '100%'}}>
+			<CardContent>
+				<CardHeader title="Actions" sx={{paddingLeft: 0, paddingTop: 0}} />
+				{isLoading ? (
+					<CircularProgress />
+				) : (
+					<>
+						<Typography color={'text.secondary'}>Job Actions</Typography>
+						<Actions
+							actions={job?.actions.filter((action) => !action.isRoleAction)}
+							onClick={appendAction}
+						/>
+						<Typography color={'text.secondary'}>Role Actions</Typography>
+						<Actions
+							actions={job?.actions.filter((action) => action.isRoleAction)}
+							onClick={appendAction}
+						/>
+					</>
+				)}
+			</CardContent>
+		</Card>
 	);
 };
 
