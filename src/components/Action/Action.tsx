43cb59ec386ba8e2d8aaa112fa3a9fc04bba54fd
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2021-10-27 23:35:54 +0100

    feat: standardise rotation action distances (#35)

--- src/components/Action/Action.tsx
+++ src/components/Action/Action.tsx
@@ -1,5 +1,4 @@
 import {Box, Tooltip} from '@mui/material';
-import {Theme} from '@mui/material/styles';
 import type {BoxProps, SxProps} from '@mui/system';
 import {Image} from 'mui-image';
 import type {FC} from 'react';
@@ -8,19 +7,24 @@ import xivIcon from 'util/xivIcon';
 
 export const DEFAULT_ACTION_SIZE = 40;
 
-type Variant = 'default' | 'readonly';
+type Variant = 'default' | 'readonly' | 'empty' | 'hidden';
 
 type Props = {
-	action: XIVAction;
+	action?: XIVAction;
 	size?: number;
 	variant?: Variant;
 	duration?: number;
 } & BoxProps;
 
-const boxStyle: SxProps<Theme> = {
+const sharedStyle: SxProps = {
 	position: 'relative',
 	userSelect: 'none',
 	borderRadius: 1,
+	boxShadow: 1,
+};
+
+const actionStyle: SxProps = {
+	...sharedStyle,
 	'&:before': {
 		position: 'absolute',
 		content: '" "',
@@ -29,6 +33,7 @@ const boxStyle: SxProps<Theme> = {
 		top: 0,
 		left: 0,
 		borderRadius: 1,
+		zIndex: 1,
 		background: [
 			'-webkit-gradient(linear, left top, left bottom, color-stop(0%, rgba(255, 255, 255, 0.6)), color-stop(100%, rgba(255, 255, 255, .15)))',
 			'-moz-linear-gradient(top, rgba(255, 255, 255, 0.6) 0%, rgba(255, 255, 255, .15) 100%)',
@@ -43,18 +48,32 @@ const boxStyle: SxProps<Theme> = {
 		left: 0,
 		borderRadius: 1,
 		border: 1,
-		boxShadow: 1,
+		zIndex: 2,
 	},
 };
 
 const defaultVariant: SxProps = {
+	...actionStyle,
 	cursor: 'pointer',
 	':hover': {
 		opacity: 0.5,
 	},
 };
 
-const readonlyVariant: SxProps = {};
+const emptyVariant: SxProps = {
+	...sharedStyle,
+	background: 'black',
+	opacity: 0.25,
+	border: 1,
+};
+const readonlyVariant: SxProps = {
+	...actionStyle,
+	opacity: 0.5,
+};
+
+const hiddenVariant: SxProps = {
+	opacity: 0,
+};
 
 type VariantStyle = {
 	[x in Variant]: SxProps;
@@ -63,6 +82,8 @@ type VariantStyle = {
 const styles: VariantStyle = {
 	default: defaultVariant,
 	readonly: readonlyVariant,
+	empty: emptyVariant,
+	hidden: hiddenVariant,
 };
 
 const Action: FC<Props> = ({
@@ -72,12 +93,23 @@ const Action: FC<Props> = ({
 	variant = 'default',
 	duration = 500,
 	...props
-}) => (
-	<Tooltip title={action.name} disableInteractive>
-		<Box sx={{...boxStyle, ...sx, height: size, width: size, ...styles[variant]}} {...props}>
-			<Image src={xivIcon(action.iconHD)} width={size} height={size} duration={duration} />
-		</Box>
-	</Tooltip>
-);
+}) => {
+	if (!action && (variant === 'empty' || variant === 'hidden')) {
+		return (
+			<Box
+				sx={{...styles[variant], ...sx, height: size, width: size, minHeight: size, minWidth: size}}
+				{...props}
+			/>
+		);
+	}
+
+	return (
+		<Tooltip title={action?.name ?? ''} disableInteractive>
+			<Box sx={{...styles[variant], ...sx, height: size, width: size}} {...props}>
+				<Image src={xivIcon(action?.iconHD ?? '')} width={size} height={size} duration={duration} />
+			</Box>
+		</Tooltip>
+	);
+};
 
 export default Action;
--- src/components/Rotation/Rotation.tsx
+++ src/components/Rotation/Rotation.tsx
@@ -1,7 +1,8 @@
 import {Stack} from '@mui/material';
 import type {SxProps} from '@mui/system';
 import Action, {DEFAULT_ACTION_SIZE} from 'components/Action';
-import type {FC} from 'react';
+import type {FC, ReactElement} from 'react';
+import {Fragment} from 'react';
 import type {TimelineXIVAction} from 'util/types';
 
 type Props = {
@@ -15,29 +16,51 @@ type Props = {
 const Rotation: FC<Props> = ({
 	actions,
 	size = DEFAULT_ACTION_SIZE,
-	oGcdRatio = 0.8,
-	oGcdOffset = 1,
+	oGcdRatio = 0.75,
 	onActionClick = () => {},
 }) => {
+	const ogcdSize = size * oGcdRatio;
 	const gcdStyle: SxProps = {
-		marginBottom: 1,
-		marginTop: oGcdOffset,
+		marginBottom: 2,
+		marginTop: 1,
 	};
 
+	const icons: ReactElement[] = actions.map((action, index) => {
+		const icon = (
+			<Action
+				key={index}
+				action={action}
+				sx={action.onGCD ? gcdStyle : {}}
+				size={action.onGCD ? size : ogcdSize}
+				duration={250}
+				onClick={() => {
+					onActionClick(action, index);
+				}}
+			/>
+		);
+
+		const spacers = [];
+		if (action.gcdType === 'gcd' && actions[index - 1]?.gcdType === 'gcd') {
+			spacers.push(<Action variant="hidden" size={ogcdSize} />);
+			spacers.push(<Action variant="hidden" size={ogcdSize} />);
+		} else if (
+			action.gcdType === 'ogcd' &&
+			actions[index - 1]?.gcdType === 'gcd' &&
+			actions[index - 1]?.gcdType === 'gcd'
+		) {
+			spacers.push(<Action variant="hidden" size={ogcdSize} />);
+		}
+		return (
+			<Fragment key={index}>
+				{spacers}
+				{icon}
+			</Fragment>
+		);
+	});
+
 	return (
-		<Stack direction="row" gap={0.25} flexWrap="wrap">
-			{actions.map((action, index) => (
-				<Action
-					key={index}
-					action={action}
-					sx={action.onGCD ? gcdStyle : {}}
-					size={action.onGCD ? size : size * oGcdRatio}
-					duration={250}
-					onClick={() => {
-						onActionClick(action, index);
-					}}
-				/>
-			))}
+		<Stack direction="row" gap={0.25} sx={{overflowX: 'auto'}}>
+			{icons}
 		</Stack>
 	);
 };
