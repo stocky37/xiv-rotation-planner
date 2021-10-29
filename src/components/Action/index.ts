96c50e3fcaf337461599d3b11e36da98f65aba85
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2021-10-29 14:32:53 +0100

    refactor: clean up rotation actions

--- src/components/Action/index.ts
+++ src/components/Action/index.ts
@@ -1,2 +1,2 @@
 export type {ActionProps} from './Action';
-export {default, DEFAULT_ACTION_SIZE} from './Action';
+export {default, STANDARD_ACTION_SIZE} from './Action';
--- src/components/Rotation/components/RotationAction.tsx
+++ src/components/Rotation/components/RotationAction.tsx
@@ -1,23 +1,47 @@
 import {Stack, Typography} from '@mui/material';
 import {SxProps} from '@mui/system';
-import Action, {ActionProps} from 'components/Action';
+import Action, {ActionProps, STANDARD_ACTION_SIZE} from 'components/Action';
 import type {FC} from 'react';
+import {TimedXIVAction} from 'util/types';
 
 type Props = {
-	label: string;
+	action: TimedXIVAction;
+	ogcdRatio?: number;
+	gap?: number;
 } & ActionProps;
 
-const gcdSx: SxProps = {
+const gcdStyle: SxProps = {
 	marginTop: 1,
 };
 
-const ogcdSx: SxProps = {};
+const ogcdStyle: SxProps = {};
 
-const RotationAction: FC<Props> = ({action, label, ...props}) => {
+const RotationAction: FC<Props> = ({
+	size = STANDARD_ACTION_SIZE,
+	action,
+	duration = 250,
+	ogcdRatio = 0.75,
+	gap = 1,
+	sx,
+	...props
+}) => {
+	const ogcdSize = size * ogcdRatio;
+	const gapSize = gap * 8; // default mui spacing, change if it is changed in theme
 	return (
-		<Stack>
-			<Action action={action} sx={action?.onGCD ? gcdSx : ogcdSx} {...props} />
-			{action?.onGCD ? <Typography textAlign="center">{label}</Typography> : null}
+		<Stack
+			sx={{
+				...sx,
+				marginLeft: `${action.ogcdGaps * (ogcdSize + gapSize)}px`,
+			}}
+		>
+			<Action
+				action={action}
+				duration={duration}
+				sx={action?.onGCD ? gcdStyle : ogcdStyle}
+				size={action.onGCD ? size : size * ogcdRatio}
+				{...props}
+			/>
+			{action?.onGCD ? <Typography textAlign="center">{action?.gcdNumber}</Typography> : null}
 		</Stack>
 	);
 };
