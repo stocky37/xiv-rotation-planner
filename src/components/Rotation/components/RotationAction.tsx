115f0c15bfb6965351201e945b4fa1e0a4c4569b
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2021-12-08 16:31:14 +0000

    wip

--- src/components/Rotation/components/RotationAction.tsx
+++ src/components/Rotation/components/RotationAction.tsx
@@ -1,4 +1,5 @@
 import {Stack, Typography} from '@mui/material';
+import {Box} from '@mui/system';
 import Action, {ActionProps, ActionSizesInPx} from 'components/Action';
 import type {FC} from 'react';
 import {TimedXIVAction} from 'util/types';
@@ -22,9 +23,28 @@ const RotationAction: FC<Props> = ({
 				...sx,
 				marginLeft: `${action.ogcdGaps * (ActionSizesInPx[size] + gap * 8)}px`,
 				marginTop: action?.onGCD ? `${ActionSizesInPx[size] / 2}px` : 0,
+				position: 'relative',
+				marginRight: '40px',
 			}}
 		>
-			<Action action={action} duration={duration} {...props} />
+			<Box
+				sx={{
+					'&:after': {
+						position: 'absolute',
+						content: '""',
+						display: 'block',
+						zIndex: 0,
+						background: `red`,
+						height: `6px`,
+						width: `40px`,
+						top: '20px',
+						right: '-40px',
+						borderRadius: '1px',
+					},
+				}}
+			>
+				<Action action={action} duration={duration} {...props} />
+			</Box>
 			{action?.onGCD ? <Typography textAlign="center">{action?.gcdNumber}</Typography> : null}
 		</Stack>
 	);
