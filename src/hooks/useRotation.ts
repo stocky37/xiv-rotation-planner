46bc033cf0d6e7ec1f5af73b7100c5b7315533bc
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2022-03-20 19:50:39 +0000

    refactor: update for new rotation api (#76)

--- src/hooks/useRotation.ts
+++ src/hooks/useRotation.ts
@@ -1,10 +1,13 @@
 import type {Rotation} from 'api/types';
 import {UseQueryResult} from 'react-query';
+import {useRecoilState} from 'recoil';
+import {rotationAtom} from 'util/atoms';
 
 import useFetchRotation from './api/useFetchRotation';
-import useRotationActions from './useRotationActions';
+import useJob from './useJob';
 
 export default function useRotation(): UseQueryResult<Rotation> {
-	const [actions] = useRotationActions();
-	return useFetchRotation({actions});
+	const [job] = useJob();
+	const [rotation] = useRecoilState(rotationAtom);
+	return useFetchRotation({job, rotation});
 }
